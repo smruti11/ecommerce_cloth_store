c02c7dcac38e057a797e4c9d191d10a29374e0d2
smruti swain <smruti.avanti@gmail.com>
2024-03-17 10:28:19 +0530

    footer

--- src/customer/Pages/HomePage/HomePage.jsx
+++ src/customer/Pages/HomePage/HomePage.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import MainCarosel from '../../HomeCarosel/MainCarosel';
 import HomeSectionCarosel from '../../components/HomeSectionCarosel/HomeSectionCarosel';
+import { mens_kurta } from '../../../Data/mens_kurta';
 
 const HomePage = () => {
   return (
     <div>
         <MainCarosel />
-        <div>
-        <HomeSectionCarosel />
+        <div className='py-20 space-y-10 flex flex-col justify-center px-5 lg:px-10'>
+        <HomeSectionCarosel data ={mens_kurta} sectionName={"Men's Kurta"}/>
+        <HomeSectionCarosel data ={mens_kurta} sectionName={"Men's Shoes"}/>
+        <HomeSectionCarosel data ={mens_kurta} sectionName={"Men's Shirt"}/>
+        <HomeSectionCarosel data ={mens_kurta} sectionName={"Women's Saree"}/>        
+        <HomeSectionCarosel data ={mens_kurta} sectionName={"Women's Dress"}/>
         </div>
     </div>
   )
--- src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
+++ src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
@@ -1,27 +1,77 @@
-import React from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
+import React, { useState } from "react";
+import AliceCarousel from "react-alice-carousel";
+import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
+import { Button } from "@mui/material";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 
-const HomeSectionCarosel = () => {
-    const responsive = {
-        0: { items: 1 },
-        720: { items: 2},
-        1024: { items: 5}
-    }
 
-    const items =[1,1,1,1,1,1].map((item)=><HomeSectionCard />);
+const HomeSectionCarosel = ({data, sectionName}) => {
+
+    const [activeIndex, setActiveIndex] =  useState(0);
+
+  const responsive = {
+    0: { items: 1 },
+    720: { items: 2 },
+    1024: { items: 5.5 },
+  };
+  const slidePrev = () => setActiveIndex(activeIndex - 1);
+
+  const slideNext = () => setActiveIndex(activeIndex + 1);
+
+  const syncActiveIndex = ({item}) => setActiveIndex(item);
+
+  const items = data.slice(0,10).map((item) => <HomeSectionCard product={item}/>);
   return (
-    <div>
+    <div className="border">
+        <h2 className="text-2xl font-extrabold text-gray-800 py-5">{sectionName}</h2>
+      <div className="relative p-5">
         <AliceCarousel
-        items={items}
-        disableButtonsControls
-        autoPlay
-        autoPlayInterval={1000}
-        infinite
-        responsive={responsive}
-    />
-    </div>
-  )
-}
+          items={items}
+          disableButtonsControls
+          responsive={responsive}
+          disableDotsControls
+          onSlideChanged={ syncActiveIndex }
+          activeIndex={activeIndex}
+        />
+       {activeIndex !== items.length-5 && <Button
+          variant="contained"
+          className="z-50 bg-white"
+          aria-label="next"
+          onClick={ slideNext }
+          sx={{
+            position: "absolute",
+            top: "8rem",
+            left: "0rem",
+            transform: "translateX(50%) rotate(90deg)",
+            bgcolor: "white",
+          }}          
+        >
+          <KeyboardArrowLeftIcon
+            sx={{ transform: "rotate(-90deg)", color: "black" }}
+          />
+        </Button> }
 
-export default HomeSectionCarosel
+       { activeIndex !== 0 && <Button
+         onClick={ slidePrev }
+          variant="contained"
+          className="z-50 bg-white"
+          aria-label="next"
+          sx={{
+            position: "absolute",
+            top: "8rem",
+            right: "0rem",
+            transform: "translateX(-50%) rotate(90deg)",
+            bgcolor: "white",
+          }}
+         
+        >
+          <KeyboardArrowLeftIcon
+            sx={{ transform: "rotate(90deg)", color: "black" }}
+          />
+        </Button> }
+      </div>
+    </div>
+  );
+};
+
+export default HomeSectionCarosel;
