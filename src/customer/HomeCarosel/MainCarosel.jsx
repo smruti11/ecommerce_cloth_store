c16663ebc24085dc92c4e6d21b54ac036141f559
smruti swain <smruti.avanti@gmail.com>
2024-03-19 23:00:19 +0530

    navigation update

--- src/customer/HomeCarosel/MainCarosel.jsx
+++ src/customer/HomeCarosel/MainCarosel.jsx
@@ -5,7 +5,7 @@ import { mainCarouselData } from './MainCaroselData';
 
 const MainCarosel = () => {
     const items = mainCarouselData.map((item) => <img src={item.image} role='presentation' 
-    className='cursor-pointer' alt=""/>)
+    className='cursor-pointer -z-10' alt=""/>)
   return  <AliceCarousel
         mouseTracking
         items={items}
